af451b14337ffcc894094e22dac1ddbc815377be
Ghazwan Ababil <ababilghazwan@gmail.com>
2024-10-11 07:21:21 +0700

    Pertanyaan Percobaaan 1

--- SiakadFor11.java
+++ SiakadFor11.java
@@ -4,6 +4,7 @@ public class SiakadFor11 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double nilai, tertinggi = 0, terendah = 100;
+        int lulus=0,tidakLulus=0;
 
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
@@ -15,9 +16,16 @@ public class SiakadFor11 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+            if (nilai >= 60) {
+                lulus++;
+            } else {
+                tidakLulus++;
+            }
         }
         sc.close();
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Jumlah mahasiswa yang lulus: " + lulus);
+        System.out.println("Jumlah mahasiswa yang tidak lulus: " + tidakLulus);
     }   
 }
